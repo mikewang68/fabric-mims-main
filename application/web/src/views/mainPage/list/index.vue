950e018203615351f1a82bb3a29472689b5aa668
Zlcaj <142517608+Zlcaj@users.noreply.github.com>
2023-09-13 12:03:46 +0800

    Add files via upload
    改动了系统前端的页面，新增了几张图片。

--- application/web/src/views/mainPage/list/index.vue
+++ application/web/src/views/mainPage/list/index.vue
@@ -7,7 +7,7 @@
 
     <el-card class="mainPage-card" >
       <div slot="header" class="mainPageHeader" style="text-align: center;">
-        项目说明
+        基于区块链的医疗信息管理系统
       </div>
 
       <div class="mainPageNote">
@@ -41,6 +41,53 @@
       </div><br>
 
     </el-card>
+    
+    <el-row  class="home-item">
+          <el-col :xs="16" :sm="8" :md="4" :lg="4">
+            <el-card :body-style="{ padding: '40px' }">
+              <div class="img" >
+                <img src="./home1.png" alt="" class="image">
+              </div>
+              <div>
+                <h3 style="padding: 10px;text-align: center;color: #62aff1">就诊追踪</h3>
+                <p style="text-align: center">追踪患者就诊记录，负责、透明</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="16" :sm="8" :md="4" :lg="4">
+            <el-card :body-style="{ padding: '40px' }">
+              <div class="img" >
+                <img src="./home2.png" alt="" class="image">
+              </div>
+              <div>
+                <h3 style="padding: 10px;text-align: center;color: #62aff1">药房管理</h3>
+                <p style="text-align: center">管理药房各类药品订单，库存</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="16" :sm="8" :md="4" :lg="4">
+            <el-card :body-style="{ padding: '40px' }">
+              <div class="img" >
+                <img src="./home3.png" alt="" class="image">
+              </div>
+              <div>
+                <h3 style="padding: 10px;text-align: center;color: #62aff1">住院管理</h3>
+                <p style="text-align: center">完善，详细记录患者住院记录</p>
+              </div>
+            </el-card>
+          </el-col>
+          <el-col :xs="16" :sm="8" :md="4" :lg="4">
+            <el-card :body-style="{ padding: '40px' }">
+              <div class="img" >
+                <img src="./home4.png" alt="" class="image">
+              </div>
+              <div>
+                <h3 style="padding: 10px;text-align: center;color: #62aff1">医药采购</h3>
+                <p style="text-align: center">详细，入账各类药品采购记录</p>
+              </div>
+            </el-card>
+          </el-col>
+        </el-row>
 
 
   </div>
@@ -84,5 +131,21 @@ export default {
 
   .mainPage-card .mainPageNote {
     font-size: 16px;
+  }
+  .home-item{
+   justify-content: space-between;
+   display: flex;
+   margin-top: 20px;
+}
+  .img{
+    background: #EFF5FD;
+    height: 124px;
+    width: 124px;
+    border-radius: 50%;
+  }
+  .image {
+    width: 200px;
+    height: 150px;
+    display: block;
   }
 </style>
