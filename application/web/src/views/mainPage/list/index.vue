66f212331b121a8dcf44a37d21bdfb01c3379813
Zlcaj <142517608+Zlcaj@users.noreply.github.com>
2023-09-16 14:48:02 +0800

    Add files via upload
    更改主页页面

--- application/web/src/views/mainPage/list/index.vue
+++ application/web/src/views/mainPage/list/index.vue
@@ -5,42 +5,7 @@
       <p>用户名: {{ account_name }}</p>
     </el-alert>
 
-    <el-card class="mainPage-card" >
-      <div slot="header" class="mainPageHeader" style="text-align: center;">
-        基于区块链的医疗信息管理系统
-      </div>
-
-      <div class="mainPageNote">
-        <el-tag>系统角色: </el-tag>
-        <span style="margin-left: 5px;"> {{ '管理员、医生、患者、药店、保险机构' }}</span>
-      </div><br>
-
-      <div class="mainPageNote">
-        <el-tag type="success">医生功能: </el-tag>
-        <span style="margin-left: 5px;"> {{ '查看患者信息、创建病历、查看病历' }}</span>
-      </div><br>
-
-      <div class="mainPageNote">
-        <el-tag type="warning">患者功能: </el-tag>
-        <span style="margin-left: 5px;"> {{ '查看病历、查看药品订单、查看保险报销申请、发起保险报销申请' }}</span>
-      </div><br>
-
-      <div class="mainPageNote">
-        <el-tag type="success">药店功能: </el-tag>
-        <span style="margin-left: 5px;"> {{ '查看病历、创建药品订单、查看药品订单' }}</span>
-      </div><br>
-
-      <div class="mainPageNote">
-        <el-tag>保险机构功能: </el-tag>
-        <span style="margin-left: 5px;"> {{ '查看病历、查看保险报销记录、创建保险报销记录、审核患者发起的报销申请' }}</span>
-      </div><br>
-
-      <div class="mainPageNote">
-        <el-tag type="danger">管理员: </el-tag>
-        <span style="margin-left: 5px;"> {{ '查看各类记录、查看账号、创建账号' }}</span>
-      </div><br>
-
-    </el-card>
+    
     
     <el-row  class="home-item">
           <el-col :xs="16" :sm="8" :md="4" :lg="4">
