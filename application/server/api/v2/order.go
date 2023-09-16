884c348012bb6b99fd63b4115e6b179a23a1bf3d
Zlcaj <142517608+Zlcaj@users.noreply.github.com>
2023-09-16 15:05:45 +0800

    Add files via upload
    新增go文件

---
+++ application/server/api/v2/order.go
@@ -0,0 +1,97 @@
+package v2
+
+import (
+	bc "application/blockchain"
+	"application/pkg/app"
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"net/http"
+)
+
+// type RealEstateRequestBody struct {
+// 	AccountId   string  `json:"accountId"`   //操作人ID
+// 	Proprietor  string  `json:"proprietor"`  //所有者(客户)(客户AccountId)
+// 	TotalArea   float64 `json:"totalArea"`   //总面积
+// 	LivingSpace float64 `json:"livingSpace"` //生活空间
+// }
+
+type RealOrderRequestBody struct {
+	AccountId   string `json:"accountId"`  //操作人ID
+	Proprietor  string `json:"proprietor"` //所有者(客户)(客户AccountId)
+	Carmodel    string `json:"carmodel"`
+	Car_scz     string `json:"car_scz"`
+	Car_scd     string `json:"car_scd"`
+	Car_scsj    string `json:"car_scsj"`
+	Car_lbjph   string `json:"car_lbjph"`
+	Car_lbjscz  string `json:"car_lbjscz"`
+	//Car_lbjscd  string `json:"car_lbjscd"`
+	//Car_lbjscsj string `json:"car_lbjscsj"`
+}
+
+type RealOrderQueryRequestBody struct {
+	Proprietor string `json:"proprietor"` //所有者(客户)(客户AccountId)
+}
+
+func CreateOrder(c *gin.Context) {
+	appG := app.Gin{C: c}
+	body := new(RealOrderRequestBody)
+	//解析Body参数
+	if err := c.ShouldBind(body); err != nil {
+		appG.Response(http.StatusBadRequest, "失败", fmt.Sprintf("参数出错%s", err.Error()))
+		return
+	}
+
+	var bodyBytes [][]byte
+	bodyBytes = append(bodyBytes, []byte(body.AccountId))
+	bodyBytes = append(bodyBytes, []byte(body.Proprietor))
+	bodyBytes = append(bodyBytes, []byte(body.Carmodel))
+	bodyBytes = append(bodyBytes, []byte(body.Car_scz))
+	bodyBytes = append(bodyBytes, []byte(body.Car_scd))
+	bodyBytes = append(bodyBytes, []byte(body.Car_scsj))
+	bodyBytes = append(bodyBytes, []byte(body.Car_lbjph))
+	bodyBytes = append(bodyBytes, []byte(body.Car_lbjscz))
+	//bodyBytes = append(bodyBytes, []byte(body.Car_lbjscd))
+	//bodyBytes = append(bodyBytes, []byte(body.Car_lbjscsj))
+
+	//调用智能合约
+	resp, err := bc.ChannelExecute("createOrder", bodyBytes)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, "失败", err.Error())
+		return
+	}
+	var data map[string]interface{}
+	if err = json.Unmarshal(bytes.NewBuffer(resp.Payload).Bytes(), &data); err != nil {
+		appG.Response(http.StatusInternalServerError, "失败", err.Error())
+		return
+	}
+	appG.Response(http.StatusOK, "成功", data)
+}
+
+func QueryOrder(c *gin.Context) {
+	appG := app.Gin{C: c}
+	body := new(RealOrderQueryRequestBody)
+	//解析Body参数
+	if err := c.ShouldBind(body); err != nil {
+		appG.Response(http.StatusBadRequest, "失败", fmt.Sprintf("参数出错%s", err.Error()))
+		return
+	}
+	var bodyBytes [][]byte
+	if body.Proprietor != "" {
+		bodyBytes = append(bodyBytes, []byte(body.Proprietor))
+	}
+	//调用智能合约
+	resp, err := bc.ChannelQuery("queryRealEstateList", bodyBytes)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, "失败", err.Error())
+		return
+	}
+	// 反序列化json
+	var data []map[string]interface{}
+	if err = json.Unmarshal(bytes.NewBuffer(resp.Payload).Bytes(), &data); err != nil {
+		appG.Response(http.StatusInternalServerError, "失败", err.Error())
+		return
+	}
+	appG.Response(http.StatusOK, "成功", data)
+}
